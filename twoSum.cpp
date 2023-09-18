d41e0c238e9c6ee17f4cf175212827acd0392c00
Enfariches <serg-eburg2@yandex.ru>
2023-09-18 23:13:12 +0500

    first commit

---
+++ twoSum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    
+    vector<int> output;
+    auto iter = nums.begin();
+
+    while(iter != nums.end())
+    {
+        int number = target - *iter;
+        auto nextIter = iter;
+        auto it = find(nextIter + 1, nums.end(), number);
+
+        if(it != nums.end())
+        {
+            output.push_back(iter - nums.begin());
+            output.push_back(it - nums.begin());
+        }
+            
+        ++iter;
+    }
+    
+    return output;
+
+}
